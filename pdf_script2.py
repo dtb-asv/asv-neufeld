040f17c2cb814d2e096871e0d65aca8893ecf67f
ASV Neufeld <asv@neufeld.at>
2026-04-24 13:52:00 +0200

    Update Kalender

--- pdf_script2.py
+++ pdf_script2.py
@@ -14,7 +14,7 @@ background_path = "background.jpg"
 
 
 modus = "single"  # "single" oder "alle"
-ziel_datum_input = "2026-04-21"
+ziel_datum_input = "2026-04-20"
 if ziel_datum_input:
     ziel_datum = datetime.strptime(ziel_datum_input, "%Y-%m-%d")
 else:
